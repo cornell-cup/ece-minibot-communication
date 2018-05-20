973014487dafb93f9839c510efae6bf6924657db
ahd59 <ahd59@github.com>
2018-05-19 23:44:12 -0400

    color

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@
 Tests for minibot movement.
 """
 from minibot.bot import Bot
+from minibot.hardware.arduino.gpio import start, stop
 
 import json
 
@@ -14,23 +15,5 @@ if __name__ == "__main__":
     bot = Bot(config)
     motor= bot.get_actuator_by_name("demo")
     motor.set_speed(50,50)
-    color= bot.get_sensor_by_name("color")
-    while 1:
-        print(color.read())
-    '''
-    while true:
-        print("Moving until BLUE")
-        bot.move_forward(50)
-        while color.get_color_name() != "BLUE":
-           print("...")
-           bot.wait(0.001)
-        bot.stop()
-        bot.wait(1)
-        print("Turning off BLUE")
-        bot.turn_clockwise(30)
-        while color.get_color_name() == "BLUE":
-           print("...")
-           bot.wait(0.001)
-        bot.stop()
-        bot.wait(1)
-    '''
+
+    start(.75)
