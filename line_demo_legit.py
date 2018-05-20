32a507edb8417a680123e075f68e26930f9c4ae1
ahd59 <ahd59@github.com>
2018-05-20 15:31:50 -0400

    line

--- line_demo_legit.py
+++ line_demo_legit.py
@@ -18,35 +18,34 @@ if __name__ == "__main__":
     motor.set_speed(50,50)
     print("Following line until intersection.")
     drive= 1
-	lm = 3
-	rm = 5
-	spd = 225
-	a = 80
-	b = 160
-	c = 225
+    lm = 3
+    rm = 5
+    spd = 225
+    a = 80
+    b = 160
+    c = 225
     while drive:
-    	r3 = !digitalRead(11)
-    	r2 = !digitalRead(13)
-    	r1 = !digitalRead(14)
-    	l1 = !digitalRead(15)
-    	l2 = !digitalRead(16)
-    	l3 = !digitalRead(17)
-
-    	if l1+l2+l3+r1+r2+r3 >= 5:
-      		drive = 0
-      		bot.stop()
-      	else if (l1 && r1):
-      		bot.set_wheel_power(spd,spd)
-	    else if (l1):
-      		bot.set_wheel_power(spd-a,spd)
-	    else if (r1):
-      		bot.set_wheel_power(spd,spd-a)
-	    else if (l2):
-      		bot.set_wheel_power(spd-b,spd)
-	    else if (r2):
-      		bot.set_wheel_power(spd,spd-b)
-	    else if (l3):
-      		bot.set_wheel_power(spd-c,spd)
-	    else if (r3):
-      		bot.set_wheel_power(spd,spd-c)
-	    
+        r3 = !digitalRead(11)
+        r2 = !digitalRead(13)
+        r1 = !digitalRead(14)
+        l1 = !digitalRead(15)
+        l2 = !digitalRead(16)
+        l3 = !digitalRead(17)
+        if l1+l2+l3+r1+r2+r3 >= 5:
+            drive = 0
+            bot.stop()
+          else if (l1 && r1):
+            bot.set_wheel_power(spd,spd)
+        else if (l1):
+            bot.set_wheel_power(spd-a,spd)
+        else if (r1):
+            bot.set_wheel_power(spd,spd-a)
+        else if (l2):
+            bot.set_wheel_power(spd-b,spd)
+        else if (r2):
+            bot.set_wheel_power(spd,spd-b)
+        else if (l3):
+            bot.set_wheel_power(spd-c,spd)
+        else if (r3):
+            bot.set_wheel_power(spd,spd-c)
+        
