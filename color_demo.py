25a6e81f9b069ca517fc1c0d453f7aefdbc3adec
ahd59 <ahd59@github.com>
2018-05-20 17:40:21 -0400

    line

--- color_demo.py
+++ color_demo.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     while 1:
         if (color.get_color_name() == "BLUE"):
             bot.turn_clockwise(50)
-            bot.delay(0.75)
+            bot.wait(0.75)
         else:
             bot.move_forward(50)
